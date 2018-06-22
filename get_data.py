328f81dfd6ba6a9b7868ca2c1563a7139064e424
novdov <rpwjd56@gmail.com>
2018-06-22 23:12:33 +0900

    EDA/데이터 로드 함수

---
+++ get_data.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+from util import categorize_kda
+
+
+class PrepareData(object):
+    def __init__(self, data):
+        self.data = data.copy()
+
+    def _padding_inf(self):
+        max_kda = max(self.data.loc[self.data['kda'] != np.inf]['kda'])
+        idx = self.data['kda'] == np.inf
+        self.data.loc[idx, 'kda'] = max_kda
+        return self.data
+
+    def _categorize_kda(self):
+        self.data['kda_range'] = self.data['kda'].apply(categorize_kda)
+        return self.data
+
+    def _label_binarize(self):
+        lb = LabelBinarizer()
+        result_binarized = lb.fit_transform(self.data['result'])
+        self.data['result'] = result_binarized
+        return self.data
+
+    def prepare_data(self):
+        self.data = self._padding_inf()
+        self.data = self._categorize_kda()
+        self.data = self._label_binarize()
+        self.data = pd.get_dummies(self.data, columns=['kda_range'])
+
+        X = self.data.iloc[:, 1:]
+        y = self.data['result']
+        return X, y
--- util.py
+++ util.py
@@ -4,3 +4,19 @@ def text2time(timestamp):
     time_converted = float(time_split[0]) + (float(time_split[-1]) / 60)
     return round(time_converted, 2)
 
+
+def categorize_kda(x):
+    if 0 <= x < 1:
+        x = 0
+    elif 1 <= x < 2:
+        x = 1
+    elif 2 <= x < 3:
+        x = 2
+    elif x >= 3:
+        x = 3
+    return x
+
+
+
+
+
